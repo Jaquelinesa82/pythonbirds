e9d66bd01b06b4470f39319733491b351ae90c08
jacke <jaquelinesa.82@gmail.com>
2020-05-07 20:59:11 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,5 +1,7 @@
 class Pessoa:
-    def __init__(self, *filhos, nome=None, idade=33):
+    olhos = 2
+
+    def __init__(self, *filhos, nome=None, idade=33, ):
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
@@ -8,20 +10,26 @@ class Pessoa:
         return f'Olá {id(self)}'
 
 if __name__ == ' main ':
-    jaque = Pessoa(nome='Jaque')
-    maria = Pessoa(jaque, nome='Maria')
+    jaque = Pessoa(nome='jaque')
+    maria = Pessoa(jaque, nome='maria')
     print(Pessoa.cumprimentar(maria))
     print(id(maria))
     print(maria.cumprimentar())
     print(maria.nome)
     print(maria.idade)
-
     for filho in maria.filhos:
         print(filho.nome)
-    maria.sobrenome = 'Ramalho'
-    del  maria.filhos
+    maria.sobrenome = 'Santos'
+    del maria.filhos
+    maria.olhos = 1
+    del maria.olhos
     print(maria.__dict__)
     print(jaque.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(maria.olhos)
+    print(jaque.olhos)
+    print(id(Pessoa.olhos), id(maria.olhos), id(jaque.olhos))
 
 
 
