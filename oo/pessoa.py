979b1381a9cb703adf74575ba03147d8ec255034
jacke <jaquelinesa.82@gmail.com>
2020-05-07 23:14:27 -0300

    Implementada a direção

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -9,6 +9,13 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estativo():
+        return 42
+    @classmethod
+    def nome_e_atributo_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
 if __name__ == ' main ':
     jaque = Pessoa(nome='jaque')
     maria = Pessoa(jaque, nome='maria')
@@ -30,6 +37,8 @@ if __name__ == ' main ':
     print(maria.olhos)
     print(jaque.olhos)
     print(id(Pessoa.olhos), id(maria.olhos), id(jaque.olhos))
+    print(Pessoa.metodo_estativo(), maria.metodo_estativo())
+    print(Pessoa.nome_e_atributo_de_classe(), maria.nome_e_atributo_de_classe())
 
 
 
